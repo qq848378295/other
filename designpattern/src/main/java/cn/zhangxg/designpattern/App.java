74a5fb101e03058279736e9310923158da3d13a3
zhangxiguan <liuhaihua0309>
2019-09-10 18:37:41 +0800

    x

--- designpattern/src/main/java/cn/zhangxg/designpattern/App.java
+++ designpattern/src/main/java/cn/zhangxg/designpattern/App.java
@@ -1,13 +1,19 @@
 package cn.zhangxg.designpattern;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		Thread.sleep(5000);
+		String s="Hello World!";
+		List<String> list=new ArrayList<String>();
+		for(int i=0;i<3000;i++) {
+			String s1=new String();
+			System.out.println(s1);
+			list.add(s1.intern());
+			Thread.sleep(200);
+		}
+		
+	}
 }
