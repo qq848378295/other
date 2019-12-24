e23d598c51f4c7c73e9ca00efe0fad2e08acded6
zhangxiguan <liuhaihua0309>
2019-12-24 15:43:48 +0800

    init

---
+++ commonutil/src/main/java/T.java
@@ -0,0 +1,77 @@
+
+import org.apache.commons.io.FileUtils;
+import org.zxg.utils.runtime.MyRunTimeUtil;
+
+import java.io.*;
+import java.nio.charset.Charset;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class T {
+
+
+    public static void main(String[] args) throws Exception {
+        //D:\CNTV\Download\CNTV\《恋上北海道》第66集日本文化-角色扮演
+        String path = "D:\\CNTV\\Download\\CNTV\\《恋上北海道》第66集日本文化-角色扮演\\";
+        long l1 = System.currentTimeMillis();
+        saveAndExecCmd(path);
+        long l2 = System.currentTimeMillis();
+        System.out.println("耗时:" + (l2 - l1));
+    }
+
+    private static void saveAndExecCmd(String path)  {
+        Map<String, String> map = getSortList(path);
+        String name = map.get("name");
+        StringBuffer sb = new StringBuffer();
+        sb.append("cd ").append(path).append("\n").append(path,0,path.indexOf(":") + 1).append("\n"); // 跳转到目录下
+        StringBuffer concat = new StringBuffer();
+        for (int i = 1; i <= Integer.valueOf(map.get("count")); i++) {
+            String mp4 = String.format(name, i);
+            String ts = mp4 + ".ts";
+            sb.append(" ffmpeg -i " + mp4 + " -vcodec copy -acodec copy -vbsf h264_mp4toannexb " + ts).append("\n"); //先转成普通ts 文件 然后拼接
+            if (i == 1) {
+                concat.append(ts);
+            } else {
+                concat.append("|").append(ts);
+            }
+        }
+        sb.append("ffmpeg -i \"concat:").append(concat.toString()).append("\" -acodec copy -vcodec copy -absf aac_adtstoasc output.mp4").append("\n").append("exit");
+        try {
+            File file=new File(System.getProperty("java.io.tmpdir") +"/"+ UUID.randomUUID().toString()+".cmd");
+            System.out.println(file.getAbsolutePath());
+            FileUtils.write(file,sb.toString(), Charset.forName("gbk"));
+            MyRunTimeUtil.execCmdWithResult("cmd.exe /c start "+file.getAbsolutePath());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 得到正确排序后的文件
+     *
+     * @param filePath
+     * @return
+     */
+    public static Map<String, String> getSortList(String filePath) {
+        File file = new File(filePath);
+        Set<String> set = new TreeSet(Comparator.reverseOrder());
+        String name = null;
+        for (File f : file.listFiles()) {
+            if (f.isFile() && f.getName().endsWith("mp4")) {
+                set.add(f.getName());
+                if (name == null) {
+                    name = f.getName();
+                }
+            }
+        }
+        //获取通用的格式
+        int lastIndex = name.lastIndexOf("-");
+        int lastIndex2 = name.lastIndexOf(".");
+        name = name.substring(0, lastIndex + 1) + "%d" + name.substring(lastIndex2);
+        Map<String, String> map = new HashMap<>();
+        map.put("count", String.valueOf(set.size()));
+        map.put("name", name);
+        return map;
+    }
+}
---
+++ commonutil/src/main/java/org/zxg/utils/runtime/MyRunTimeUtil.java
@@ -0,0 +1,67 @@
+package org.zxg.utils.runtime;
+
+import org.apache.commons.exec.*;
+
+import java.io.*;
+
+/**
+ * https://blog.csdn.net/u011411069/article/details/78570125
+ * 参考
+ */
+public class MyRunTimeUtil {
+
+    /**
+     * 执行不需要返回结果的命令
+     * @throws Exception
+     */
+    public static void execCmdWithoutResult() throws Exception{
+        //开启windows telnet: net start telnet
+        //注意：第一个空格之后的所有参数都为参数
+        CommandLine cmdLine = new CommandLine("net");
+        cmdLine.addArgument("start");
+        cmdLine.addArgument("telnet");
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setExitValue(1);
+        //设置60秒超时，执行超过60秒后会直接终止
+        ExecuteWatchdog watchdog = new ExecuteWatchdog(60 * 1000);
+        executor.setWatchdog(watchdog);
+        DefaultExecuteResultHandler handler = new DefaultExecuteResultHandler();
+        executor.execute(cmdLine, handler);
+        //命令执行返回前一直阻塞
+        handler.waitFor();
+    }
+
+    public static void main(String[] args) throws Exception {
+        String command = "java \n javac";
+        String result=execCmdWithResult(command);
+        System.out.println(result);;
+    }
+    /**
+     * 带返回结果的命令执行
+     * @return
+     */
+    public static String execCmdWithResult(String command ) {
+        try {
+            //接收正常结果流
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            //接收异常结果流
+            ByteArrayOutputStream errorStream = new ByteArrayOutputStream();
+            CommandLine commandline = CommandLine.parse(command);
+            DefaultExecutor exec = new DefaultExecutor();
+            exec.setExitValues(null);
+            //设置一分钟超时
+            ExecuteWatchdog watchdog = new ExecuteWatchdog(60*1000);
+            exec.setWatchdog(watchdog);
+            PumpStreamHandler streamHandler = new PumpStreamHandler(outputStream,errorStream);
+            exec.setStreamHandler(streamHandler);
+            exec.execute(commandline);
+            //不同操作系统注意编码，否则结果乱码
+            String out = outputStream.toString("GBK");
+            String error = errorStream.toString("GBK");
+            return out+error;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}
